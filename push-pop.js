1e80ef60b007fbd07b0ddb7d83108a940125dd60
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-28 23:43:12 +0600

    push-pop

---
+++ push-pop.js
@@ -0,0 +1,20 @@
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+let placeList = ['Kuakata', 'Cox-Bazar','Sitakundo']; //Declaring an array.
+
+console.log(placeList); //Printing all elements of the array.
+
+placeList.push('Gutia Masque'); // Adding another tourist place in the array.
+console.log(placeList); 
+
+placeList.push('Lalbag', 'Sainmartin island'); //Adding two new element in the array.
+console.log(placeList); 
+
+placeList.pop();
+console.log(placeList);
+
