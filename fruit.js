e97e84283629a2b1570f6306b53d6b7de8c5d8a5
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-28 23:44:01 +0600

    declaring array

--- fruit.js
+++ fruit.js
@@ -1,4 +1,8 @@
-// Declare a simple array and perform different kinds of operations:
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
 
 let fruitItems = ['Apple', 'Orange', 'Banana', 'Jack Fruit', 'Mango'];
 console.log(fruitItems); //Here I have printed all elements of the array.
