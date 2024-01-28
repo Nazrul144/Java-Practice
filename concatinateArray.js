80762145ab5172b9bb89a8c0d41d08bbd045d395
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-29 00:18:55 +0600

    concatinateArray

---
+++ concatinateArray.js
@@ -0,0 +1,15 @@
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+let villageFriends = ['Maheb', 'Rakib', 'Sharif', 'Raju', 'Shahidul'];
+let cityFriends = ['Maruf', 'Joy', 'Suit', 'Jamil', 'Arman'];
+
+console.log(villageFriends); //Village friends
+console.log(cityFriends); //City friends
+
+let villageAndCity = villageFriends.concat(cityFriends); // Here I have concatinated two arrays together with concat() function.
+console.log(villageAndCity);
