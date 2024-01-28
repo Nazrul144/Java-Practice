e231f872abf53244d38478a36c3f5024eb98f216
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-28 23:28:11 +0600

    first commit

---
+++ fruit.js
@@ -0,0 +1,13 @@
+// Declare a simple array and perform different kinds of operations:
+
+let fruitItems = ['Apple', 'Orange', 'Banana', 'Jack Fruit', 'Mango'];
+console.log(fruitItems); //Here I have printed all elements of the array.
+console.log(fruitItems[3]); // Here I have printed the third element of the array.
+
+// Replacing 2nd element of the array and placing there another items.
+
+let index = 2;
+let newItems = 'Strawberry';
+
+fruitItems[index] = newItems; // Here I have added new item "Strawberry" in the place of 2nd index of the array.
+console.log(fruitItems);
