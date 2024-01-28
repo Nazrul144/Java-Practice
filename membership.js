d0b212da065e46c2c55de3081506143cfcbee9b6
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-28 23:56:43 +0600

    booksList

---
+++ membership.js
@@ -0,0 +1,15 @@
+// 3. Checking Array Membership with ‘includes’
+// Instructions:
+
+// Create an array of books containing different book.
+
+// Use the includes method to check if the array contains a javascript book.
+
+// Print a message to the console indicating whether the element is present in the array or not.
+
+
+let booksList = ['Bangla', 'English','Math', 'Physics', 'Chemistry']; // Declaring an array
+console.log(booksList);
+
+let result = booksList.includes("JavaScript");
+console.log(result);
