e6d25388ddee3dfde299412d3734debcdfae159f
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-29 15:21:40 +0600

    slice.js

---
+++ slice.js
@@ -0,0 +1,6 @@
+// Divide the array by using slice method.
+let numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+let newArray = numbers.slice(2, 5); // Here from 2 index to 5 index of data will be cut.
+
+console.log(newArray) // 30, 40, 50 will be printed.
