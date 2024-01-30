1a8216f2f730d5b8ccf5f56aaea3801389e5922c
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-30 10:52:07 +0600

    multiplicationTable.js

---
+++ Loop/multiplicationTable.js
@@ -0,0 +1,9 @@
+// Creating multiplication table using for loop.
+
+let number = 5;
+let result;
+for(let i = 1; i<=10; i++){
+    result = i * number;
+    console.log(number + " * " + i + "  =  " + result);
+}
+
