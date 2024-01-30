18cd01a346ecf26c6a7d51b0df5aa842aada45b3
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-30 11:03:39 +0600

    mul-while.js

---
+++ Loop/mul-while.js
@@ -0,0 +1,10 @@
+//Create a multiplication table with while loop.
+
+let number = 10;
+let i = 1;
+
+while(i<=10){
+    let result = i * number;
+    console.log(i + " * " + number + " = " + result);
+    i++;
+}
